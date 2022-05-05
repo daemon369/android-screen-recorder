ed2e394f3a87646900bbbef2948aebd39dc24e99
daemon369 <daemon336699@gmail.com>
2022-05-06 07:05:32 +0800

    record screen

--- app/src/main/java/me/daemon/screenrecorder/demo/MainActivity.kt
+++ app/src/main/java/me/daemon/screenrecorder/demo/MainActivity.kt
@@ -6,43 +6,23 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Point
-import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.util.DisplayMetrics
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val mediaRecord by lazy { MediaRecorder() }
     private val mediaProjectionManager by lazy { getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
-    private var mediaProjection: MediaProjection? = null
-    private var virtualDisplay: VirtualDisplay? = null
-
     private var started = false
-
-    private val callback = object : MediaProjection.Callback() {
-        override fun onStop() {
-            mediaRecord.stop()
-            mediaRecord.release()
-            virtualDisplay?.release()
-            virtualDisplay = null
-            mediaProjection?.apply {
-                stop()
-            }
-            mediaProjection = null
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,63 +45,25 @@ class MainActivity : AppCompatActivity() {
             windowManager.defaultDisplay.getRealSize(size)
 
             if (!started) {
-                mediaRecord.setAudioSource(MediaRecorder.AudioSource.MIC)
-                mediaRecord.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-                mediaRecord.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    mediaRecord.setOutputFile(
-                        File(getExternalFilesDir(null), "video.mp4")
-                    )
-                }
-                mediaRecord.setVideoSize(size.x, size.y)
-                mediaRecord.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-                mediaRecord.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-                mediaRecord.setVideoEncodingBitRate(512 * 1000)
-                mediaRecord.setVideoFrameRate(30)
-                val rotation = windowManager.defaultDisplay.rotation
-//                val orientation: Int = ORIENTATIONS.get(rotation + 90)
-//                mediaRecord.setOrientationHint(orientation)
-                mediaRecord.prepare()
-
-                if (mediaProjection == null) {
-                    startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), 1);
-                }
+                val captureIntent = mediaProjectionManager.createScreenCaptureIntent()
+                startActivityForResult(captureIntent, 1)
             } else {
-                mediaRecord.stop()
-                mediaRecord.release()
-                virtualDisplay?.release()
-                virtualDisplay = null
-                mediaProjection?.apply {
-                    stop()
-                }
-                mediaProjection = null
+                stopService(Intent(this@MainActivity, ScreenRecordService::class.java))
             }
 
             started = !started
         }
     }
 
+    @Suppress("OVERRIDE_DEPRECATION", "DEPRECATION")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (1 == requestCode && Activity.RESULT_OK == resultCode) {
-            val displayMetrics = DisplayMetrics()
-            windowManager.defaultDisplay.getRealMetrics(displayMetrics)
-
-            mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data!!).apply {
-                registerCallback(callback, null)
-                virtualDisplay = createVirtualDisplay(
-                    "MainActivity",
-                    displayMetrics.widthPixels,
-                    displayMetrics.heightPixels,
-                    displayMetrics.densityDpi,
-                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                    mediaRecord.surface,
-                    null,
-                    null
-                );
-            }
-
-            mediaRecord.start()
+            val intent2 = Intent(this@MainActivity, ScreenRecordService::class.java)
+            intent2.putExtra("resultCode", resultCode);
+            intent2.putExtra("data", data);
+            startForegroundService(intent2);
         }
     }
 }
---
+++ app/src/main/java/me/daemon/screenrecorder/demo/ScreenRecordService.kt
@@ -0,0 +1,140 @@
+package me.daemon.screenrecorder.demo
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.AudioAttributes
+import android.media.AudioPlaybackCaptureConfiguration
+import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.IBinder
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import java.io.File
+
+
+class ScreenRecordService : Service() {
+
+    companion object {
+        private const val TAG = "ScreenRecordService"
+        private const val NOTIFICATION_CHANNEL_ID = "ScreenRecordService_nofity"
+        private const val NOTIFICATION_CHANNEL_NAME = "ScreenRecordService"
+        private const val NOTIFICATION_CHANNEL_DESC = "ScreenRecordService"
+        private const val NOTIFICATION_ID = 1000
+        private const val NOTIFICATION_TICKER = "RecorderApp"
+    }
+
+    private val mediaProjectionManager by lazy { getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
+    private var mediaProjection: MediaProjection? = null
+    private var audioPlaybackCaptureConfiguration: AudioPlaybackCaptureConfiguration? = null
+
+    private var virtualDisplay: VirtualDisplay? = null
+    private val mediaRecorder = MediaRecorder()
+
+    private var resultCode: Int = -1
+    private var resultData: Intent? = null
+
+    private var running = false
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate() {
+        super.onCreate()
+        createNotification()
+
+        val displayMetrics = DisplayMetrics()
+        this.display!!.getRealMetrics(displayMetrics)
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        val f = File(getExternalFilesDir(null), "video.mp4")
+        mediaRecorder.setOutputFile(f.absolutePath)
+        mediaRecorder.setVideoSize(displayMetrics.widthPixels, displayMetrics.heightPixels)
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecorder.setVideoEncodingBitRate(2 * 1920 * 1080)
+        mediaRecorder.setVideoFrameRate(18)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        resultCode = intent?.getIntExtra("resultCode", -1) ?: -1
+        resultData = intent?.getParcelableExtra("data")
+        Log.i(TAG, "onStartCommand: $resultCode")
+        Log.i(TAG, "onStartCommand: $resultData")
+        createNotification()
+
+        val displayMetrics = DisplayMetrics()
+        this.display!!.getRealMetrics(displayMetrics)
+
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, resultData!!)
+        virtualDisplay = mediaProjection?.createVirtualDisplay(
+            "ScreenRecordService",
+            displayMetrics.widthPixels,
+            displayMetrics.heightPixels,
+            displayMetrics.densityDpi,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            mediaRecorder.surface,
+            null,
+            null
+        )
+
+        mediaRecorder.start()
+
+        Log.i(TAG, "onStartCommand: $mediaProjection")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val builder = AudioPlaybackCaptureConfiguration.Builder(mediaProjection!!)
+            builder.addMatchingUsage(AudioAttributes.USAGE_MEDIA)
+            builder.addMatchingUsage(AudioAttributes.USAGE_ALARM)
+            builder.addMatchingUsage(AudioAttributes.USAGE_GAME)
+            audioPlaybackCaptureConfiguration = builder.build()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        running = false;
+        super.onDestroy()
+    }
+
+    private fun createNotification() {
+        Log.i(TAG, "notification: " + Build.VERSION.SDK_INT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationIntent = Intent(this, ScreenRecordService::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+            val notificationBuilder: NotificationCompat.Builder =
+                NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                    .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                            resources,
+                            R.drawable.ic_launcher_foreground
+                        )
+                    )
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("Screen recorder service")
+                    .setContentText("Screen recorder service")
+                    .setTicker(NOTIFICATION_TICKER)
+                    .setContentIntent(pendingIntent)
+            val notification: Notification = notificationBuilder.build()
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = NOTIFICATION_CHANNEL_DESC
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            startForeground(NOTIFICATION_ID, notification)
+        }
+    }
+
+}
